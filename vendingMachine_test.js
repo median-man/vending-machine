c7ce2f2a24ec964a49f8b1fb62829eeb1e18472c
John Desrosiers <johndrunner@yahoo.com>
2017-10-09 05:57:49 -0700

    add js files for vendingMachine and tests

---
+++ vendingMachine_test.js
@@ -0,0 +1,8 @@
+/* This file contains all tests for code in vendingMachine.js. */
+/* globals test */
+
+// Tests for getChange function
+test('getChange(1,1) should equal [] - an empty array', () => {
+	
+});
+
