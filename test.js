1643c912fd3f3491c59d10ac6bd97123c3c2b882
John Desrosiers <johndrunner@yahoo.com>
2017-10-09 08:55:50 -0700

    rename file

--- test.js
+++ test.js
@@ -1,28 +1,26 @@
 /* This file contains all tests for code in vendingMachine.js. */
-/* globals QUnit getChange */
-
-const { assert, test } = QUnit;
+/* globals test getChange */
 
 // Tests for getChange function
-test('getChange(1,1) should equal [] - an empty array', () => {
+test('getChange(1,1) should equal [] - an empty array', (assert) => {
 	const result = getChange(1, 1); // no change
 	const expected = [];
 	assert.deepEqual(result, expected);
 });
 
-test('getChange(215, 300) should return [50, 20, 10, 5]', function() {
+test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
 	const result = getChange(215, 300);
 	const expected = [50,20,10,5];
 	assert.deepEqual(result, expected);
 });
 
-test('getChange(486, 600) should return [100, 10, 2, 2]', function() {
+test('getChange(486, 600) should return [100, 10, 2, 2]', function(assert) {
 	const result = getChange(486, 600);
 	const expected = [100, 10, 2, 2];
 	assert.deepEqual(result, expected);
 });
 
-test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function() {
+test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
 	const result = getChange(12, 400);
 	const expected = [200, 100, 50, 20, 10, 5, 2, 1];
 	assert.deepEqual(result, expected);
