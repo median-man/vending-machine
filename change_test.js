35c2de5ceed49a5ff00d67b9e9df256dbbb4eeda
John Desrosiers <johndrunner@yahoo.com>
2017-10-09 06:51:47 -0700

    add test

--- change_test.js
+++ change_test.js
@@ -21,3 +21,9 @@ test('getChange(486, 600) should return [100, 10, 2, 2]', function() {
 	const expected = [100, 10, 2, 2];
 	assert.deepEqual(result, expected);
 });
+
+test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function() {
+	const result = getChange(12, 400);
+	const expected = [200, 100, 50, 20, 10, 5, 2, 1];
+	assert.deepEqual(result, expected);
+});
